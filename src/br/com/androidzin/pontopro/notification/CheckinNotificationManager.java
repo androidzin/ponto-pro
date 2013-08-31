2d52902641aecf4cd8fd0880330d76981a16e182
Bruno Mateus <brunomateus@gmail.com>
2013-08-31 12:52:05 -0300

    WorkdayComplete notificaiton: More tests added
    Description: Test using sharedPreference
    * Schedule test
    * Cancel test
    * Full test(schedule, receiver, notify)
    
    Commit:Add
    
    TaskNumber:#4

--- src/br/com/androidzin/pontopro/notification/CheckinNotificationManager.java
+++ src/br/com/androidzin/pontopro/notification/CheckinNotificationManager.java
@@ -1,8 +1,5 @@
 package br.com.androidzin.pontopro.notification;
 
-import br.com.androidzin.pontopro.R;
-import br.com.androidzin.pontopro.model.Checkin.CheckinListener;
-import br.com.androidzin.pontopro.model.Checkin.CheckinType;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -10,7 +7,15 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import br.com.androidzin.pontopro.MainActivity;
+import br.com.androidzin.pontopro.R;
+import br.com.androidzin.pontopro.model.Checkin.CheckinListener;
+import br.com.androidzin.pontopro.model.Checkin.CheckinType;
+import br.com.androidzin.pontopro.settings.BusinessHourCommom;
 
 public class CheckinNotificationManager extends BroadcastReceiver implements CheckinListener{
 
@@ -18,17 +23,23 @@ public class CheckinNotificationManager extends BroadcastReceiver implements Che
 	public static final String WORKDAY_COMPLETE = 
 			"br.com.androidzin.pontopro.notification.CheckinNotificationManager.WORKDAY_COMPLETE";
 	
+	private SharedPreferences sharedPreferences;
+	private AlarmManager alarmManager; 
+	
 	public CheckinNotificationManager(){
 		
 	}
 	
 	public CheckinNotificationManager(Context context){
 		mContext = context;
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+		alarmManager =	(AlarmManager) mContext.getSystemService(mContext.ALARM_SERVICE);
 	}
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Notification notification = null;
+		
 		if(intent.getAction().equals(WORKDAY_COMPLETE)){
 			notification = getWorkdayCompleteNotification(context);
 		}
@@ -45,10 +56,23 @@ public class CheckinNotificationManager extends BroadcastReceiver implements Che
 	}
 
 	private Notification getNotification(Context context, String title, String message) {
+		
+		Intent resultIntent = new Intent(context, MainActivity.class);
+		PendingIntent resultPendingIntent =
+		    PendingIntent.getActivity(
+		    context,
+		    0,
+		    resultIntent,
+		    PendingIntent.FLAG_UPDATE_CURRENT
+		);
+		
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-		Notification notification = builder.setContentTitle(title).
-				setContentText(message).
-				setSmallIcon(android.R.drawable.sym_action_chat).getNotification();
+		Notification notification = builder
+				.setContentTitle(title)
+				.setContentText(message)
+				.setSmallIcon(android.R.drawable.sym_action_chat)
+				.setContentIntent(resultPendingIntent)
+				.getNotification();
 		
 		notification.flags |= Notification.FLAG_AUTO_CANCEL;
 		
@@ -69,22 +93,16 @@ public class CheckinNotificationManager extends BroadcastReceiver implements Che
 	@Override
 	public void onCheckinDone(CheckinType checkin, long when, long workedHours) {
 
-		AlarmManager alarmManager = 
-				(AlarmManager) mContext.getSystemService(mContext.ALARM_SERVICE);
-		
 		Intent intent = null;
 		PendingIntent notifier = null;
 		
 		switch(checkin){
 			case AFTER_LUNCH:
-				intent = getWorkdayCompleteIntent();
-				notifier = PendingIntent.getBroadcast(mContext, 0, intent,
-						PendingIntent.FLAG_UPDATE_CURRENT);
-				long notificationHour = when + (28800000 - workedHours);
-				alarmManager.set(AlarmManager.RTC_WAKEUP, notificationHour, notifier);
+				scheduleWorkdayCompleteNotification(when, workedHours);
 			break;
 			
 			case LEAVING:
+				cancelWorkdayCompleteNotification(workedHours);
 			break;
 		}
 		
@@ -93,5 +111,33 @@ public class CheckinNotificationManager extends BroadcastReceiver implements Che
 		
 		
 		
+	}
+
+	private void cancelWorkdayCompleteNotification(long workedHours) {
+		if(workedHours < BusinessHourCommom.getWorkingTime(sharedPreferences))
+		{
+			PendingIntent notifier = getWorkdayCompletePendingIntent();
+			notifier.cancel();
+			alarmManager.cancel(notifier);
+		}
+	}
+
+	private void scheduleWorkdayCompleteNotification(long when,
+			long workedHours) {
+		
+		
+		PendingIntent notifier = getWorkdayCompletePendingIntent();
+		
+		long notificationHour = when + 
+				(BusinessHourCommom.getWorkingTime(sharedPreferences) - workedHours);
+		
+		alarmManager.set(AlarmManager.RTC_WAKEUP, notificationHour, notifier);
+	}
+
+	private PendingIntent getWorkdayCompletePendingIntent() {
+		Intent intent = getWorkdayCompleteIntent();
+		PendingIntent notifier = PendingIntent.getBroadcast(mContext, 0, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		return notifier;
 	}
 }
--- test/PontoPro/src/br/com/androidzin/pontopro/test/notification/WorkdayComplete.java
+++ test/PontoPro/src/br/com/androidzin/pontopro/test/notification/WorkdayComplete.java
@@ -1,27 +1,36 @@
 package br.com.androidzin.pontopro.test.notification;
 
+import br.com.androidzin.pontopro.MainActivity;
 import br.com.androidzin.pontopro.R;
 import br.com.androidzin.pontopro.model.Checkin;
 import br.com.androidzin.pontopro.model.Checkin.CheckinType;
 import br.com.androidzin.pontopro.notification.CheckinNotificationManager;
+import br.com.androidzin.pontopro.settings.BusinessHourCommom;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.test.AndroidTestCase;
 
 public class WorkdayComplete extends AndroidTestCase {
 	
 	private Context mContext;
 	private CheckinNotificationManager notificationManager;
-	
+	private SharedPreferences sharedPreferences;
 	@Override
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
 		mContext = getContext();
 		notificationManager = new CheckinNotificationManager(mContext);
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+		sharedPreferences.
+			edit().
+			putString(BusinessHourCommom.WORKING_TIME_KEY, mContext.getString(R.string.eight_hour_value)).
+			commit();
 	}
 	
 	public void testNotificationIntent(){
@@ -35,8 +44,8 @@ public class WorkdayComplete extends AndroidTestCase {
 	}
 	
 	public void testBasicSchedule(){
-		long when = 14400000; // four hours
-		notificationManager.onCheckinDone(CheckinType.AFTER_LUNCH, System.currentTimeMillis(), when);
+		long workedHours = 14400000; // four hours
+		notificationManager.onCheckinDone(CheckinType.AFTER_LUNCH, System.currentTimeMillis(), workedHours);
 		
 		AlarmManager alarmManager = 
 				(AlarmManager) mContext.getSystemService(mContext.ALARM_SERVICE);
@@ -45,9 +54,52 @@ public class WorkdayComplete extends AndroidTestCase {
 		PendingIntent notifier = PendingIntent.getBroadcast(mContext, 0, intent,
 				PendingIntent.FLAG_NO_CREATE);
 		
-		assertNotNull("Fuuuu", notifier);
+		assertNotNull("Schedule did not work", notifier);
 		
 		alarmManager.cancel(notifier);
+	}
+	
+	public void testCancel(){
+		long workedHours = 14400000; // four hours
+		notificationManager.onCheckinDone(CheckinType.AFTER_LUNCH, System.currentTimeMillis(), workedHours);
+
+		notificationManager.onCheckinDone(CheckinType.LEAVING, System.currentTimeMillis(), 0);
+		
+		Intent intent = notificationManager.getWorkdayCompleteIntent();
+		
+		PendingIntent notifier = PendingIntent.getBroadcast(mContext, 0, intent,
+					PendingIntent.FLAG_NO_CREATE);
+		
+		
+		assertNull("Cancel notification did not work", notifier);
+		
+	}
+	
+	public void testWorkDayCompleteNofitication(){
+		long when = System.currentTimeMillis();
+		long workingTime = BusinessHourCommom.getWorkingTime(sharedPreferences);
+		long workedHours = workingTime - 5000;
+		
+		notificationManager.onCheckinDone(CheckinType.AFTER_LUNCH, when, workedHours);
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Intent resultIntent = new Intent(mContext, MainActivity.class);
+		PendingIntent resultPendingIntent =
+		    PendingIntent.getActivity(
+		    mContext,
+		    0,
+		    resultIntent,
+		    PendingIntent.FLAG_NO_CREATE
+		);
+		
+		
+		assertNotNull("Notification is not scheduled", resultIntent);
+
 	}
 
 }
